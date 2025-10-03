b6c28dcbafc84ae5bf3251f7393ae6d06f37c3a9
Shakthi <sakthiindravarma@gmail.com>
2025-10-03 21:45:14 +0530

    cart feature

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -26,6 +26,9 @@ const ShopContextProvider = (props) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId] - 1}))
     }
 
+    
+
+
     const contextValue = {all_product, cartItems, addToCart, removeFromCart}
 
     return(
