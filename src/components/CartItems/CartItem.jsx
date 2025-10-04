fd47472c3f15a06f7a57b3a0b4518d9766a3dca6
Shakthi <sakthiindravarma@gmail.com>
2025-10-04 20:41:03 +0530

    cart summary

--- src/components/CartItems/CartItem.jsx
+++ src/components/CartItems/CartItem.jsx
@@ -6,7 +6,7 @@ import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItem = () => {
 
-    const {all_product, cartItems, removeFromCart} = useContext(ShopContext)
+    const {getTotalCartAmount, all_product, cartItems, removeFromCart} = useContext(ShopContext)
 
 
   return (
@@ -37,11 +37,11 @@ const CartItem = () => {
         })}
         <div className="cartitems-down">
           <div className="cartitems-total">
-            <h1>cart Totals</h1>
+            <h1>Cart Totals</h1>
             <div>
               <div className="cartitems-total-item">
                 <p>Subtotal</p>
-                <p>${0}</p>
+                <p>${getTotalCartAmount()}</p>
               </div>
               <hr />
               <div className="cartitems-total-item">
@@ -51,7 +51,7 @@ const CartItem = () => {
               <hr />
               <div className="cartitems-total-item">
                 <h3>Total</h3>
-                <h3>${0}</h3>
+                <h3>${getTotalCartAmount()}</h3>
               </div>
             </div>
             <button>PROCEED TO CHECKOUT</button>
